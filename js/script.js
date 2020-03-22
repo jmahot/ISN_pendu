a75ff0f3008574d485636c240fb3d7a3986df752
jmahot <julie.mahot@outlook.fr>
2020-03-22 10:47:01 +0100

    modif credit d'auteur

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var motSecret;
-        
+
 //var time=new Date();			// Date d'aujourd'hui
 var tableauCachette=new Array();	// Le tableau qui contient les lettres du mot à trouver
 //var mots=new Array();		// Le tableau qui contient tous les mots possibles
@@ -12,7 +12,7 @@ var end=false;				// true si le jeu est terminé
 // On prend un mot au hasard en fonction de la seconde en cours
 
 /*motSecret=mots[time.getSeconds() % mots.length];*/
-motSecret=mots[Math.floor(Math.random()*mots.length)] 
+motSecret=mots[Math.floor(Math.random()*mots.length)]
 //l'autre méthode hasard trop restreint, toujours la même 1ere lettre
 tailleMot=motSecret.length;
 
@@ -21,34 +21,32 @@ function changeCouleur(element,couleur){
     element.style.color=couleur;
 }
 
-//permet récupérer mot du tableau avec accent
+// Permet de récupérer le mot du tableau avec accent
 function accent(){
     for(var i=0; i<mots.length; i++){
         if (motSecret==mots[i]){
-             return tableauNone[i];
+            return tableauNone[i];
         }
-
-    }   
-
+    }
 }
-        
+
 // Gère tous les traitements à faire lorsqu'on appuie sur une touche
 function gameplay(element){
             
     // Si la couleur de fond est orange, c'est qu'on a déjà essayé - on quitte la fonction
     if(element.style.color=="orange" || end){
         return;
-    }    
+    }
     // On récupère la lettre du clavier et on met la touche en orange (pour signaler qu'elle est cliquée)
     var lettre=element.innerHTML;
     changeCouleur(element,"orange");
-            
+    
     // On met la variable trouve à false;
     var trouve=false;
     
     // On parcours chaque lettre du mot, on cherche si on trouve la lettre sélectionnée au clavier
     for(var i=0; i<tailleMot; i++) {
-            
+        
         // Si c'est le cas :
        if(tableauCachette[i].innerHTML==lettre) {
             tableauCachette[i].style.visibility='visible';	// On affiche la lettre
@@ -62,7 +60,7 @@ function gameplay(element){
         coupsManques++;
         document.images['pendu'].src="../images/penduC/pendu_"+coupsManques+".png"; // On change l'image du pendu
         document.images['compteur'].src="../images/count/count_"+coupsManques+".png"; // On change l'image du compteur à rebours
-
+        
         // Si on a raté 10 fois :
         if(coupsManques>9){
             for(var i=0; i<tailleMot; i++) tableauCachette[i].style.visibility='visible';
